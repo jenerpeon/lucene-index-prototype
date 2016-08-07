b07f07d3c04ac8dee0f10b1c15bab0ef1658cb6d
jenerpeon <l.behrens@openthinclient.com>
2016-08-07 20:40:09 +0200

    * Static search on fields "package", "class", "tags"

--- src/main/java/uni/lars/Searcher.java
+++ src/main/java/uni/lars/Searcher.java
@@ -15,6 +15,8 @@ import org.apache.lucene.store.FSDirectory;
 
 import java.io.IOException;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by lars on 8/5/16.
@@ -24,16 +26,18 @@ public class Searcher{
     IndexSearcher indexSearcher;
     Analyzer analyzer;
     QueryParser parser;
-    String field = "contents";
+    ArrayList<String> fields;
 
     private void lookString(String text) throws ParseException, QueryNodeException, IOException, org.apache.lucene.queryparser.classic.ParseException {
-        parser = new QueryParser(field, analyzer);
-        Query query = parser.parse("WATT");
-        ScoreDoc[] hits = indexSearcher.search(query, 10).scoreDocs;
-        System.out.println(hits.length);
-        for (int i = 0; i < hits.length; i++) {
-            Document hitDoc = indexSearcher.doc(hits[i].doc);
-            System.out.println("indexed Text:"+ hitDoc.get(field));
+        for(String field : fields) {
+            parser = new QueryParser(field, analyzer);
+            Query query = parser.parse(text);
+            System.out.println("Results:"+indexSearcher.count(query));
+            ScoreDoc[] hits = indexSearcher.search(query, 10).scoreDocs;
+            for (int i = 0; i < hits.length; i++) {
+                Document hitDoc = indexSearcher.doc(hits[i].doc);
+                System.out.println("Hit:" + hitDoc.get(field));
+            }
         }
     }
 
@@ -41,7 +45,6 @@ public class Searcher{
         try{
             lookString(text);
             return true;
-
         }catch (Exception e){
             System.out.println("lookup failed:"+e.getMessage());
             return false;
@@ -53,6 +56,9 @@ public class Searcher{
         Path Index = new java.io.File(Indexer.IndexLocation).toPath();
         IndexReader indexReader = DirectoryReader.open(FSDirectory.open(Index));
         this.indexSearcher = new IndexSearcher(indexReader);
+
+        fields = new ArrayList<String>();
+        fields.addAll(Arrays.asList("Package", "Parent", "Tags"));
     }
 
     public Searcher(){
